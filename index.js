01bda8c94b800ac2499f01560593de2e83fb9925
Nikita Lysenkov <Nikitalysenkov8@gmail.com>
2023-09-10 19:21:08 +0300

    Устранение конфликта2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.listen(port, function () {
 });
 
 let cors = require('cors');
-app.use(cors({ origin: 'http://31.172.67.138/' }));
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // Подключаем middleware для сессий
 app.use(
